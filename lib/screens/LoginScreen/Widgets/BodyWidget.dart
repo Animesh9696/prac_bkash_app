c0f7d5a0123ae377b2825fc7749164479f31e387
Animesh Kumar <animesh.kumar969@gmail.com>
2023-02-08 21:58:38 +0600

    separate keyboardfrom main page

--- lib/screens/LoginScreen/Widgets/BodyWidget.dart
+++ lib/screens/LoginScreen/Widgets/BodyWidget.dart
@@ -7,6 +7,8 @@ import 'package:prac_bkash_app/screens/HomeScreen/HomeScreen.dart';
 import 'package:prac_bkash_app/utilities/constants.dart';
 import 'package:prac_bkash_app/utilities/spacingWidget.dart';
 
+import '../../../widgets/KeyboardWidget.dart';
+
 class BodyWidget extends StatefulWidget {
   const BodyWidget({Key? key}) : super(key: key);
 
@@ -300,134 +302,5 @@ class _BodyWidgetState extends State<BodyWidget> {
   }
 }
 
-typedef KeybordKeyCallback = void Function(String key);
 
-class CustomKeyboard extends StatelessWidget {
-  final KeybordKeyCallback onKeybordButtonPressed;
-  final VoidCallback keyboardArrowForward;
-  final VoidCallback clearPassword;
-  const CustomKeyboard({
-    required this.onKeybordButtonPressed,
-    required this.keyboardArrowForward,
-    required this.clearPassword,
-    super.key,
-  });
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 240,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            alignment: Alignment.center,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                KeybordButton(
-                    onPress: () => onKeybordButtonPressed("1"),
-                    text: "1"),
-                KeybordButton(
-                    onPress: () => onKeybordButtonPressed("2"),
-                    text: "2"),
-                KeybordButton(
-                    onPress: () => onKeybordButtonPressed("3"),
-                    text: "3"),
-              ],
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 9),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                KeybordButton(
-                    onPress: () => onKeybordButtonPressed("4"),
-                    text: "4"),
-                KeybordButton(
-                    onPress: () => onKeybordButtonPressed("5"),
-                    text: "5"),
-                KeybordButton(
-                    onPress: () => onKeybordButtonPressed("6"),
-                    text: "6"),
-              ],
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 9),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                KeybordButton(
-                    onPress: () => onKeybordButtonPressed("7"),
-                    text: "7"),
-                KeybordButton(
-                    onPress: () => onKeybordButtonPressed("8"),
-                    text: "8"),
-                KeybordButton(
-                    onPress: () => onKeybordButtonPressed("9"),
-                    text: "9"),
-              ],
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 9),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                MaterialButton(
-                  onPressed: () {
-                    clearPassword();
-                  },
-                  color: Colors.white,
-                  textColor: Colors.black,
-                  shape: const CircleBorder(),
-                  child: const Icon(
-                    Icons.cancel,
-                    size: 30,
-                  ),
-                ),
-                KeybordButton(
-                    onPress: () => onKeybordButtonPressed("0"),
-                    text: "0"),
-                MaterialButton(
-                  onPressed: () {
-                    keyboardArrowForward();
-                  },
-                  color: Colors.pink,
-                  textColor: Colors.black,
-                  shape: const CircleBorder(),
-                  child: const Icon(Icons.arrow_forward),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class KeybordButton extends StatelessWidget {
-  final VoidCallback onPress;
-  final String text;
-  const KeybordButton({
-    super.key,
-    required this.onPress,
-    required this.text,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialButton(
-      padding: addPadding(5),
-      onPressed: onPress,
-      color: Colors.white,
-      textColor: Colors.black,
-      shape: const CircleBorder(),
-      child: Text(text,
-          style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-    );
-  }
-}
--- lib/widgets/KeyboardWidget.dart
+++ lib/widgets/KeyboardWidget.dart
@@ -1,57 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:prac_bkash_app/utilities/constants.dart';
 
-class Keyboard extends StatefulWidget {
-  const Keyboard({Key? key}) : super(key: key);
+import '../utilities/spacingWidget.dart';
 
-  @override
-  _KeyboardState createState() => _KeyboardState();
-}
 
-class _KeyboardState extends State<Keyboard> {
-  double hight = 60;
+typedef KeybordKeyCallback = void Function(String key);
+
+class CustomKeyboard extends StatelessWidget {
+  final KeybordKeyCallback onKeybordButtonPressed;
+  final VoidCallback keyboardArrowForward;
+  final VoidCallback clearPassword;
+  const CustomKeyboard({
+    required this.onKeybordButtonPressed,
+    required this.keyboardArrowForward,
+    required this.clearPassword,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 300,
+      height: 240,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
+            alignment: Alignment.center,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                KeybordButton(
+                    onPress: () => onKeybordButtonPressed("1"),
+                    text: "1"),
+                KeybordButton(
+                    onPress: () => onKeybordButtonPressed("2"),
+                    text: "2"),
+                KeybordButton(
+                    onPress: () => onKeybordButtonPressed("3"),
+                    text: "3"),
+              ],
+            ),
+          ),
+          Container(
             margin: const EdgeInsets.only(top: 9),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                MaterialButton(
-                  onPressed: () {},
-                  color: Colors.white,
-                  textColor: Colors.black,
-                  child: const Text("1",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-                  shape: const CircleBorder(),
-                  padding: const EdgeInsets.all(15),
-                ),
-                MaterialButton(
-                  onPressed: () {},
-                  color: Colors.white,
-                  textColor: Colors.black,
-                  child: const Text("2",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-                  shape: const CircleBorder(),
-                  padding: const EdgeInsets.all(15),
-                ),
-                MaterialButton(
-                  onPressed: () {},
-                  color: Colors.white,
-                  textColor: Colors.black,
-                  child: const Text("3",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-                  shape: const CircleBorder(),
-                  padding: const EdgeInsets.all(15),
-                ),
+                KeybordButton(
+                    onPress: () => onKeybordButtonPressed("4"),
+                    text: "4"),
+                KeybordButton(
+                    onPress: () => onKeybordButtonPressed("5"),
+                    text: "5"),
+                KeybordButton(
+                    onPress: () => onKeybordButtonPressed("6"),
+                    text: "6"),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 9),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                KeybordButton(
+                    onPress: () => onKeybordButtonPressed("7"),
+                    text: "7"),
+                KeybordButton(
+                    onPress: () => onKeybordButtonPressed("8"),
+                    text: "8"),
+                KeybordButton(
+                    onPress: () => onKeybordButtonPressed("9"),
+                    text: "9"),
               ],
             ),
           ),
@@ -61,116 +80,61 @@ class _KeyboardState extends State<Keyboard> {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 MaterialButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    clearPassword();
+                  },
                   color: Colors.white,
                   textColor: Colors.black,
-                  child: const Text("4",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
                   shape: const CircleBorder(),
-                  padding: const EdgeInsets.all(15),
+                  child: const Icon(
+                    Icons.cancel,
+                    size: 30,
+                  ),
                 ),
+                KeybordButton(
+                    onPress: () => onKeybordButtonPressed("0"),
+                    text: "0"),
                 MaterialButton(
-                  onPressed: () {},
-                  color: Colors.white,
+                  onPressed: () {
+                    keyboardArrowForward();
+                  },
+                  color: Colors.pink,
                   textColor: Colors.black,
-                  child: const Text("5",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
                   shape: const CircleBorder(),
-                  padding: const EdgeInsets.all(15),
-                ),
-                MaterialButton(
-                  onPressed: () {},
-                  color: Colors.white,
-                  textColor: Colors.black,
-                  child: const Text("6",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-                  shape: const CircleBorder(),
-                  padding: const EdgeInsets.all(15),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 9),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                MaterialButton(
-                  onPressed: () {},
-                  color: Colors.white,
-                  textColor: Colors.black,
-                  child: const Text("7",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-                  shape: const CircleBorder(),
-                  padding: const EdgeInsets.all(15),
-                ),
-                MaterialButton(
-                  onPressed: () {},
-                  color: Colors.white,
-                  textColor: Colors.black,
-                  child: const Text("8",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-                  shape: const CircleBorder(),
-                  padding: const EdgeInsets.all(15),
-                ),
-                MaterialButton(
-                  onPressed: () {},
-                  color: Colors.white,
-                  textColor: Colors.black,
-                  child: const Text("9",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-                  shape: const CircleBorder(),
-                  padding: const EdgeInsets.all(15),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 9),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                MaterialButton(
-                  onPressed: () {},
-                  color: Colors.white,
-                  textColor: Colors.black,
-                  child: const Text("X",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-                  shape: const CircleBorder(),
-                  padding: const EdgeInsets.all(15),
-                ),
-                MaterialButton(
-                  onPressed: () {},
-                  color: Colors.white,
-                  textColor: Colors.black,
-                  child: const Text("0",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-                  shape: const CircleBorder(),
-                  padding: const EdgeInsets.all(15),
-                ),
-                MaterialButton(
-                  onPressed: () {},
-                  color: Colors.white,
-                  textColor: Colors.black,
-                  child: const Text("Go",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-                  shape: const CircleBorder(),
-                  padding: const EdgeInsets.all(15),
+                  child: const Icon(Icons.arrow_forward),
                 ),
               ],
             ),
           ),
         ],
       ),
+    );
+  }
+
+
+
+}
+
+
+class KeybordButton extends StatelessWidget {
+  final VoidCallback onPress;
+  final String text;
+  const KeybordButton({
+    super.key,
+    required this.onPress,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      padding: addPadding(5),
+      onPressed: onPress,
+      color: Colors.white,
+      textColor: Colors.black,
+      shape: const CircleBorder(),
+      child: Text(text,
+          style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
     );
   }
 }
